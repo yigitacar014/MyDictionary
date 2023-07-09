3885cbc8e7f8e8796abd990dbc118e205bce49b9
munir <munir@DESKTOP-PDO8257>
2023-07-09 22:04:17 +0300

    Add project files.

---
+++ MyDictionary/Mylist.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    internal class Mylist<T>
+    {
+        public void Add(T item )
+        {
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+List<string> kitaplar = new List <string>();
+kitaplar.Add("Polisiye");
+kitaplar.Add("Macera");
+kitaplar.Add("Aksiyon");
+kitaplar.Add("Psikoloji");
+
+foreach (var item in kitaplar)
+{
+    Console.WriteLine(item);
+}
